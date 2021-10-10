01ed4b97733525ec5b5689432cb384c2e74b358e
James <jamesmca@live.com>
2021-10-09 21:09:39 -0700

    Update main.py
    calling to api

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ import requests
 import nest_asyncio
 import asyncpg
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('.env')
 nest_asyncio.apply()
-# %% keys
-API_KEY = os.getenv('API_KEY')
 
+# %% KEYS
+API_KEY = os.getenv('API_KEY')
+CHANNEL_ID = "UCTckA2i1O6aiqdnsYm7jhnQ"
+
+#make API call
+pageToken = ''
+url = 'https://www.googleapis.com/youtube/v3/search?key='+API_KEY+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=10000"+pageToken
+response = requests.get(url).json()
+response['items']
+# %%
