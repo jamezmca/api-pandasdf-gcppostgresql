7cb76b4a21f850e420ae84b879383d83adc54323
James <jamesmca@live.com>
2021-10-11 23:17:34 -0700

    prices upload finished
    data officially uploaded to first table (stock prices)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # %% Import libararies
+from datetime import datetime
 import os
 import numpy as np
 import pandas as pd
@@ -9,6 +10,7 @@ import asyncio
 import nest_asyncio
 import asyncpg
 from dotenv import load_dotenv
+from datetime import datetime
 from pytrends.request import TrendReq
 load_dotenv('.env')
 nest_asyncio.apply()
@@ -18,11 +20,13 @@ nest_asyncio.apply()
 # url = 'https://www.googleapis.com/youtube/v3/search?key='+API_KEY+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=10000"+pageToken
 # response = requests.get(url).json()
 
+#%% GET DATA - CSV > POSTGRESQL > REFETCH AND RECLEAN
+
 #%% GET DATA FROM YFINANCE
 def clenseArray(array):
     array = [x.lower().replace(" ", "_")\
-        .replace("-","_").replace("?","_").replace(r"/", "_")\
-        .replace(")", "").replace(r"(", "").replace("%", "")\
+        .replace("-","_").replace("?","_").replace(r"/", "_").replace('.', '')\
+        .replace(")", "").replace(r"(", "").replace("%", "").replace('all', 'all_')\
         .replace("?", "").replace("\\", "_").replace("$","").replace('&',"and") for x in array]
     return array
 
@@ -40,8 +44,8 @@ df_sp_values = yf.download(sp_ticker_list, start="2016-01-01")
 #%% TAKE ADJ CLOSE VALUES AND TURN INTO DF
 df_sp_prices = df_sp_values['Adj Close']
 df_sp_prices.columns = [x.lower().replace(" ", "_")\
-        .replace("-","_").replace("?","_").replace(r"/", "_")\
-        .replace(")", "").replace(r"(", "").replace("%", "")\
+        .replace("-","_").replace("?","_").replace(r"/", "_").replace('.', '')\
+        .replace(")", "").replace(r"(", "").replace("%", "").replace('all', 'all_')\
         .replace("?", "").replace("\\", "_").replace("$","").replace('&',"and") for x in df_sp_prices.columns]
 
 #%% PYTRENDS DATA
@@ -70,24 +74,19 @@ df_sp_searches.columns = [x.lower().replace(" ", "_")\
         .replace(")", "").replace(r"(", "").replace("%", "")\
         .replace("?", "").replace("\\", "_").replace("$","").replace('&',"and") for x in df_sp_searches.columns]
 
-#%% A GOOD CHECK TO MAKE SURE DATAFRAMES ALIGN
-# df_sp_searches['abbott_laboratories']
-df_sp_searches.axes[0][0]
-print(f'{df_sp_searches.axes[0][0]}'.split(' ')[0])
 
-df_sp_prices.loc['2019-08-04']
-# df_sp_prices.loc[f'{df_sp_searches.axes[0][10]}'.split(' ')[0]]
+#%%
+df_sp_prices
+#%% SAVE TO CSV FILE
+df_sp_searches.to_csv('df_sp_searches.csv', header=df_sp_searches.columns, index=True , encoding='utf-8')
+df_sp_prices.to_csv('df_sp_prices.csv', header=df_sp_prices.columns, index=True , encoding='utf-8')
+
 
 #%%CREATE TABLE SCHEMA
 col_str = 'date DATE, '
 for stock_label in df_sp_prices.columns:
     col_str = col_str + f'{stock_label} ' + 'FLOAT, ' 
-
-col_str_searches = 'date DATE, '
-for stock_label in df_sp_searches.columns:
-    col_str_searches = col_str_searches + f'{stock_label} ' + 'FLOAT, '
-col_str
-
+col_str = col_str[:-2]
 
 #%% UPLOAD DATA TO POSTGRESQL DATABASE IN GOOGLE CLOUD
 #USER AUTH FOR GOOGLE CLOUD DATABASE FROM ENVIRONMENT VARIABLES
@@ -96,10 +95,21 @@ password = os.getenv("PASSWORD")
 database = os.getenv("DATABASE")
 ip = os.getenv("PUBLICIP")
 
-def makeListOfTuples(dataframe):
-    listOfTuples = []
-    for i in range(len(dataframe)):
-        listOfTuples.append(dataframe.loc(i))
+def typeClean(str):
+    str_arr = str.strip().split(',')
+    
+    for i in range(len(str_arr)):
+        print(f'i is: {i} and: {str_arr[i]}')
+        if str[i] == '': print(i)
+        # if i == 0:
+        #     str_arr[i] = datetime.fromisoformat(str_arr[i])
+        # elif str[i] == None:
+        #     str[i] = None
+        # else:
+        #     print(str_arr[i])
+        #     str_arr[i] = float(str_arr[i])
+
+rando = 'date DATE, hump VARCHAR, cool FLOAT'
 
 async def run():
     conn = await asyncpg.connect(user=user, password=password, database=database, host=ip)
@@ -114,11 +124,24 @@ async def run():
     print('sp_prices was created successfully')
     await conn.execute(f'''
             CREATE TABLE sp_searches (
-                {col_str_searches}
+                {col_str}
             );
         ''')
     print('sp_searches was created successfully')
+    # copy prices to table using price header
+    values = []
+    with open('df_sp_prices.csv', 'r') as f:
+        next(f)
+        for row in f:
+            values.append(tuple(typeClean(row)))
+        
+    result = await conn.copy_records_to_table(
+        'sp_prices', records=values
+    )
+    print(result, 'import to sp_prices complete')
 
+    # copy searches to table using insert if header
+    # equals dictionary translation
     await conn.close() #close the connection
 loop = asyncio.get_event_loop() #can also make single line
 loop.run_until_complete(run())
