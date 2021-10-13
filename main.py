32f2c5fae113177bda06ec4bb97273b5d3dbedb4
James <jamesmca@live.com>
2021-10-12 22:00:39 -0700

    Update main.py
    second table not working - going to create a second col_str when creating table and just mass upload data as per the first instance

--- main.py
+++ main.py
@@ -78,12 +78,21 @@ if len(csv_files) == 0:
     df_sp_prices.to_csv('df_sp_prices.csv', header=df_sp_prices.columns, index=True , encoding='utf-8')
 else: 
     df_sp_prices = pd.read_csv('df_sp_prices.csv')
-    
+    df_sp_searches = pd.read_csv('df_sp_searches.csv')
+
 #CREATE TABLE SCHEMA
 col_str = 'date DATE, '
 for stock_label in df_sp_prices.columns:
     col_str = col_str + f'{stock_label} ' + 'FLOAT, ' 
 col_str = col_str[:-2]
+
+ivd = {v: k for k, v in dictionary.items()}
+
+col_str_two = 'date DATE, '
+for stock_label in df_sp_searches.columns:
+    col_str_two = col_str_two + f'{ivd[stock_label]} ' + 'FLOAT, '
+col_str_two = col_str_two[:-2]
+
 
 #UPLOAD DATA TO POSTGRESQL DATABASE IN GOOGLE CLOUD
 #USER AUTH FOR GOOGLE CLOUD DATABASE FROM ENVIRONMENT VARIABLES
@@ -113,7 +122,6 @@ def dolStrTwo(column):
     print(string.strip()[:-1])
     return string.strip()[:-1]
 
-ivd = {v: k for k, v in dictionary.items()}
 
 async def run():
     conn = await asyncpg.connect(user=user, password=password, database=database, host=ip)
@@ -160,107 +168,54 @@ loop = asyncio.get_event_loop() #can also make single line
 loop.run_until_complete(run())
 print('all tables successfully imported')
 
-#%%
-
-def dolStr(length): 
-    string = ''
-    for i in range(length):
-        if i != length - 1: 
-            string = string + f'(${i+1}) '
-        else:
-            string = string + f'(${i+1})'
-    return string
-
-def dolStrTwo(column): 
-    string = ''
-    col = column[1:]
-    print(col)
-    for val in col:
-        string = string + f"('{val}'), "
-    print(string.strip()[:-1])
-    return string.strip()[:-1]
-
-def colToTupleList(column):
-    col = column[1:]
-    colList = []
-    if isinstance(column[1], (int, np.integer)):
-        for val in col:
-            print(val)
-            colList.append((float(val)))
-    else:
-        for val in col:
-            colList.append(f'{val}')
-    return colList
-ivd = {v: k for k, v in dictionary.items()}
-
+#%% make a copy of thing and remake table and run until it works
 async def run():
     conn = await asyncpg.connect(user=user, password=password, database=database, host=ip)
     print('connected')
+    await conn.execute(f'DROP TABLE IF EXISTS sp_searches')
+    await conn.execute(f'''
+        CREATE TABLE sp_searches (
+            {col_str}
+        );
+    ''')
+    print('sp_searches was created successfully')
 
-    valuesSecond = []
-    df_sp_searches = pd.read_csv('df_sp_searches.csv')
-    for col in df_sp_searches.columns:
-        print(col)
-        await conn.execute(f'''
-            INSERT INTO sp_searches ({ivd[col] if col != 'date' else 'date'})
-            VALUES {dolStrTwo(df_sp_searches[col].values)};
-        ''')
-
-        
-        #create ($n) string for length of column and insert array of tuples for each item 
-        # await conn.executemany(f'''
-        #     INSERT INTO sp_searches ({dictionary[col] if col != 'date' else 'date'}) VALUES {dolStr(len(df_sp_searches[col]))};
-        #     ''', colToTupleList(df_sp_searches[col].values))
-
-    await conn.close() 
-
+    await conn.close() #close the connection
 asyncio.get_event_loop().run_until_complete(run())
+print('all tables successfully imported')
+#%% practice
 
-#%%
-
-      
-
-df_sp_searches = pd.read_csv('df_sp_searches.csv')
-df_sp_searches['3m']
-dictionary['mmm']
-# cols = df_sp_searches.columns[0]
-# print(df_sp_searches[cols].values[0])
-# print(isinstance(df_sp_searches[cols].values[1], (int, np.integer)))
-# if '-' in df_sp_searches[cols].values:
-#     print('hi')
-
-
-
-
-    
-# %% practice table
+rando = {
+    'date': "('2014-09-03'), ('2020-05-07'), ('2018-03-21'), ('1999-04-06')",
+    'bananas': '(1), (2), (4), (8)',
+    'hello': '(0), (1), (1), (2)',
+    'cookie': '(1), (3), (5), (7)'
+}
 
 async def run():
     conn = await asyncpg.connect(user=user, password=password, database=database, host=ip)
     print('connected')
-
     await conn.execute(f'DROP TABLE IF EXISTS practice')
     await conn.execute(f'''
-            CREATE TABLE practice (
-                date DATE,
-                hello INT,
-                bananas INT,
-                cookie INT
-            );
+        CREATE TABLE practice (
+            date DATE,
+            bananas INT,
+            hello INT,
+            cookie INT
+        );
+    ''')
+    print('practice was created successfully')
+    for col in ['date', 'bananas', 'hello', 'cookie']:
+        print(col)
+        await conn.execute(f'''
+            INSERT INTO practice ({col})
+            VALUES {rando[col]};
         ''')
-    await conn.execute(f'''
-            INSERT INTO practice (date)
-            VALUES ('2015-06-08'), ('2015-06-10'), ('2015-06-09');
-        ''')
-
-    print('all tables successfully imported')   
-    await conn.close() 
-
+    await conn.close() #close the connection
 asyncio.get_event_loop().run_until_complete(run())
+print('all tables successfully imported')
+# %%
 
 # %%
-string = 'banana oact cookie, '
-string.strip()[:-1]
-# %%
-print(datetime.fromisoformat('2014-07-06'))
+dictionary['amzn']
 # %%
