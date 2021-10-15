67e21433db219f844eb7fa44f546cc17f0b209b3
James <jamesmca@live.com>
2021-10-14 17:09:52 -0700

    Update analysis.py
    function finds all negative gradients for a given analysis range

--- analysis.py
+++ analysis.py
@@ -10,8 +10,8 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 nest_asyncio.apply()
 #START OF NON-OPTIONAL CODE
-df_sp_prices = pd.read_csv('df_sp_prices.csv')
-df_sp_searches = pd.read_csv('df_sp_searches.csv')
+df_sp_prices = pd.read_csv('df_sp_prices.csv', 'r')
+df_sp_searches = pd.read_csv('df_sp_searches.csv', 'r')
 print('Successfully imported csv files :)')
 ##ASSUMPTIONS
 #Intraday losses greater than 20% are unlikely to be a market failure
@@ -45,32 +45,70 @@ tempDf = df_sp_prices['amzn']
 # next value and saves gradient etc until finish
 # searches object and eliminates similar entries or upgrades a min or max 
 # point if it creates a more significant gradient or a larger pecentage
-def twentyPercent(array):
+practiceArray = pd.Series([0, 1, 3, 5, 10, 7, 8, 7, 5, 11, 11, 12.5, 5, 9, 6, 15])
+#5 neg grads
+
+def twentyPercent(array, analysisRange, decrement):
     gradients = {}
 
     length = len(array)
-    print(array[0])
     for i in range(length):
-        if i > 0.95*length:
+        if i > (1-analysisRange)*length:
             break
-        tempArray = list(array[i:i+math.ceil(0.05*length)])
+        tempArray = list(array[i:i+math.ceil(analysisRange*length)])
         maximum = max(tempArray)
         maxIndex = tempArray.index(maximum)
         minimum = min(tempArray) if min(tempArray) != 0 else 0.001
-        minIndex = tempArray.index(minimum)
-        deltaX = abs(maxIndex - minIndex) if abs(maxIndex - minIndex) > 0 else 0.001
-        grad = (maximum - minimum) / deltaX
-        gradients[i] = {"max": maximum, "min": minimum, "grad": grad, "perc": maximum/minimum}
-    bigGradients = {k:v for (k,v) in gradients.items() if v['perc'] > 1.2}
-    i = 0
-    while i < len(bigGradients):
-        if bigGradients[i]['max'] <  bigGradients[i+1]['max'] and bigGradients[i]['min'] == bigGradients[i+1]['min']:
-            
+        minIndex = tempArray.index(0) if minimum == 0.001 else tempArray.index(minimum)
+        if maxIndex - minIndex != 0:
+            deltaX = maxIndex - minIndex
+        else: #can just set it to a positive integer cause I only want negative gradients
+            #and max and min are the same value
+            deltaX = 10
+        grad = (maximum - minimum) / deltaX #possibly should be perc/deltaX
+        gradients[i] = {"max": maximum, 
+                        "min": minimum, 
+                        "grad": grad, 
+                        "perc": maximum/minimum,
+                        'minIndex': minIndex,
+                        'maxIndex': maxIndex}
+    bigGradients = {k:v for (k,v) in gradients.items() if v['perc'] > decrement and v['grad'] < 0}
+    return bigGradients
 
-twentyPercent(tempDf)
+def diffInDays(start, end):
+    return
+
+def amalgamate(obj):
+    length = len(obj.keys())
+    finalObj = {}
+    def checkLeft(): 
+        #check for bigger values on left, considering not making the grad too much less
+        pass
+    def checkRight(): #check for smaller values on right, not making the grad too much less
+        pass
+        
+    prevMax = None
+    prevMin = None
+    for i in range(length):
+        key = list(obj.keys())[i]
+        val = obj[key]
+        print(val)
+
+
+
+
+    return
+
+ansisRange = 0.1
+dec = 1.2
+gradientLimit = 3 #for eg 20% in 2 months so 20%/60days
+negGrads = twentyPercent(practiceArray, ansisRange, dec)
+finalGrads = amalgamate(negGrads)
+
+
 
 # %%
-bigGradients
+practiceArray = pd.Series([0, 1, 3, 5, 10, 7, 8, 7, 5, 11, 11, 12.5, 5, 9, 6, 15])
 
 # %%
 # %%
